0af55387fe633f82716e97bd8e0380c697527a64
Miguel Di Luca <mdiluca@itba.edu.ar>
2019-04-20 19:20:57 -0300

    Cambios en spring security

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ProfileController.java
@@ -0,0 +1,28 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
+@Controller
+public class ProfileController {
+
+    @RequestMapping(value = "/username", method = RequestMethod.GET)
+    @ResponseBody
+    public String currentUserName(Principal principal) {
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        return username;
+    }
+}
